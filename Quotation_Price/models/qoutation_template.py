387ad760017014604407f046e805168b8ea3d8c7
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-18 21:36:26 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) iSoft Solutions Limited
 
-from odoo import fields, models
+from odoo import fields,models,api
 
 class SaleOrderTemplate(models.Model):
-    _inherits = "sale.order.template"
+    _inherits = 'sale.order.template'
 
     description_head = fields.Char(string='Header')
