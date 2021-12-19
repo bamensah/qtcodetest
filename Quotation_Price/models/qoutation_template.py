fcae1ab087875852aa9f90474540161cf1bd3968
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-19 17:54:21 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -8,5 +8,5 @@ class QuotationTemplateCustom(models.Model):
     _inherit = ['sale.order.template.line','sale.order.line']
     _description = "Quotation Template Custom"
 
-    price_unit = fields.Float(string="Unit Price")
-    analytic_line_ids = (string="Analytic lines")
+    testone = fields.Float(string="Unit Price")
+    testtwo = (string="Analytic lines")
