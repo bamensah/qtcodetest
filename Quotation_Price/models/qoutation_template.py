ed997c130f6d2afcb50f5ffa32ac8c0406c8b94a
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-19 16:18:49 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -3,12 +3,7 @@
 
 from odoo import fields, models
 
-class UnitPriceCustom(models.Model):
-    _name = "unit.price.custom"
-    _inherits = {"sale.order.line":"price_unit"}
-
 class QuotationTemplateCustom(models.Model):
     _inherit = "sale.order.template.line"
-    _inherit = "unit.price.custom"
 
     price_unit = fields.Float(string="Unit Price")
