42ee40a7b6b098d2f667bf2365f80b8b3de5eb26
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-20 10:58:45 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -4,7 +4,15 @@
 from odoo import api, fields, models, _
 
 class QuotationTemplateCustom(models.Model):
-    _inherit = ['sale.order.template.line','sale.order.line']
+    _inherit = "sale.order.template.line"
 
-    price_unit = fields.Float("sale.order.line", string="Unit Price")
-    
+    unit_price = fields.Float(string="Unit Price")
+
+class UnitPriceChange(models.Model):
+    _inherit = "sale.order.line"
+    quotation_template_custom_id = fields.Many2one("sale.order.template.line", string="Quotation Template Custom")
+    price_unit = fields.Float(string="Unit Price")
+
+    @api.onchange('quotation_template_custom_id')
+    def onchange_quotation_template_custom_id(self):
+        self.price_unit = self.quotation_template_custom_id.unit_price
