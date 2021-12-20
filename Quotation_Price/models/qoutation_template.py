f338001f777ce30e8b40ef30dadcef1f66b85842
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-20 06:33:47 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) iSoft Solutions Limited
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 class QuotationTemplateCustom(models.Model):
     _inherit = "sale.order.template.line"
@@ -11,10 +11,11 @@ class QuotationTemplateCustom(models.Model):
 class UnitPriceChange(models.Model):
     _inherit = "sale.order.line"
 
+    @api.model
     @api.onchange('price_unit')
     def _onchange_price(self):
         res = super(QuotationTemplateCustom, self)._onchange_price()
 
         for rec in self:
-            rec.price_unit = unit_price
+            rec.price_unit = self.unit_price
         return res
