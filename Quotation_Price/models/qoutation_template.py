206e42b93feadb002408fa7722514d715e314c85
iSoft Solutions Ghana <ben.anyanmensah@gmail.com>
2021-12-21 09:50:49 +0000

    Update qoutation_template.py

--- Quotation_Price/models/qoutation_template.py
+++ Quotation_Price/models/qoutation_template.py
@@ -9,10 +9,12 @@ class SaleOrderTemplateLine(models.Model):
     unit_price = fields.Float(string='Unit Price')
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+    _inherit = "sale.order"
 
-    @api.onchange('product_id')
-    def SetUnitPrice(self):
-        if self.product_id:
-            self.name = self.product_id.display_name
-            self.price_unit = self.product_id.unit_price
+    @api.onchange('sale_order_template_id')
+    def onchange_sale_order_template_id(self):
+        ret = super(SaleOrder, self).onchange_sale_order_template_id()
+        if self.sale_order_template_id:
+            template_price = self.sale_order_template_id.with_context(lang=self.product_id.unit_price)
+            self.price_unit = template.unit_price
+            return ret
